0131e92f622780c1245fa470d9d837316745494c
p1r2s-seth <77240227+p1r2s-seth@users.noreply.github.com>
2023-06-29 15:05:38 +0530

    country page

--- index.js
+++ index.js
@@ -127,3 +127,28 @@ var submitted=false;
 
 // })
 
+//CONTACT FORM
+function closeForm() {
+  $('.form-popup-bg').removeClass('is-visible');
+}
+
+$(document).ready(function($) {
+  
+  /* Contact Form Interactions */
+  $('#btnOpenForm').on('click', function(event) {
+    event.preventDefault();
+
+    $('.form-popup-bg').addClass('is-visible');
+  });
+  
+    //close popup when clicking x or off popup
+  $('.form-popup-bg').on('click', function(event) {
+    if ($(event.target).is('.form-popup-bg') || $(event.target).is('#btnCloseForm')) {
+      event.preventDefault();
+      $(this).removeClass('is-visible');
+    }
+  });
+  
+  
+  
+  });
